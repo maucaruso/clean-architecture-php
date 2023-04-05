68d90b0b67b24e056c72f008ab0ed9546182909d
maucaruso <carusojr@outlook.com>
2023-04-05 09:05:27 -0300

    test: CPF and Email tests

---
+++ tests/CPFTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PHP\Architecture\Tests;
+
+use PHP\Architecture\CPF;
+use PHPUnit\Framework\TestCase;
+
+class CpfTest extends TestCase
+{
+  public function testCpfNumeroNoFormatoInvalidoNaoDevePoderExistir()
+  {
+    $this->expectException(\InvalidArgumentException::class);
+    new CPF('12345678910');
+  }
+
+  public function testCpfDevePoderSerRepresentadoComoString()
+  {
+    $cpf = new CPF('123.456.789-10');
+    $this->assertSame('123.456.789-10', (string) $cpf);
+  }
+}
---
+++ tests/EmailTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PHP\Architecture\Tests;
+
+use PHP\Architecture\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+  public function testEmailNoFormatoInvalidoNaoDevePoderExistir()
+  {
+    $this->expectException(\InvalidArgumentException::class);
+    new Email('email inválido');
+  }
+
+  public function testEmailDevePoderSerRepresentadoComoString()
+  {
+    $email = new Email('endereco@example.com');
+    $this->assertSame('endereco@example.com', (string) $email);
+  }
+} 
