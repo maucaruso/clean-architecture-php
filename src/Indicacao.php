103a98a6246c4928201b93644b83843b1f0b9e51
maucaruso <carusojr@outlook.com>
2023-04-06 08:53:58 -0300

    feat: Indicacao entity

---
+++ src/Indicacao.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PHP\Architecture;
+
+class Indicacao
+{
+  private Aluno $indicante;
+  private Aluno $indicado;
+  private \DateTimeImmutable $data;
+  
+  public function __construct(Aluno $indicante, Aluno $indicado)
+  {
+    $this->indicante = $indicante;
+    $this->indicado = $indicado;
+    
+    $this->data = new \DateTimeImmutable();
+  }
+}
