29e56a6cc5aeeda87e61796fa84dda8e29168f95
maucaruso <carusojr@outlook.com>
2023-04-11 09:16:04 -0300

    feat: implementing buscarPorCpf method in Aluno repository

--- src/Infra/Aluno/RepositorioDeAlunoComPdo.php
+++ src/Infra/Aluno/RepositorioDeAlunoComPdo.php
@@ -3,6 +3,7 @@
 namespace PHP\Architecture\Infra\Aluno;
 
 use PHP\Architecture\Dominio\Aluno\Aluno;
+use PHP\Architecture\Dominio\Aluno\AlunoNaoEncontrado;
 use PHP\Architecture\Dominio\Aluno\RepositorioDeAluno;
 use PHP\Architecture\Dominio\CPF;
 
@@ -35,7 +36,41 @@ class RepositorioDeAlunoComPdo implements RepositorioDeAluno
     }
   }
 
-  public function buscarPorCpf(CPF $cpf): Aluno { }
+  public function buscarPorCpf(CPF $cpf): Aluno {
+    $sql = '
+      SELECT cpf, nome, email, ddd, numero as numero_telefone
+      FROM alunos
+      LEFT JOIN telefones ON telefones.cpf_aluno = alunos.cpf
+      WHERE alunos.pf = ?;
+    ';
+    
+    $stmt = $this->conexao->prepare($sql);
+    $stmt->bindValue(1, (string) $cpf);
+    $stmt->execute();
+    
+    $dadosAluno = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    
+    if (count($dadosAluno) === 0) {
+      throw new AlunoNaoEncontrado($cpf);
+    }
+    
+    return $this->mapearAluno($dadosAluno);
+  }
+  
+  private function mapearAluno($dadosAluno): Aluno
+  {
+    $primeiraLinha = $dadosAluno[0];
+    $aluno = Aluno::comCpfNomeEEmail($primeiraLinha['cpf'], $primeiraLinha['nome'], $primeiraLinha['email']);
+    $telefones = array_filter($dadosAluno, fn ($linha) => $linha['ddd'] !== null && $linha['numero_telefone'] !== null);
+    
+    foreach ($telefones as $linha) {
+      $aluno->adicionarTelefone($linha['ddd'], $linha['numero_telefone']);
+    }
+    
+    return $aluno;
+  }
 
-  public function buscarTodos(): array { }
+  public function buscarTodos(): array {
+    
+  }
 }
