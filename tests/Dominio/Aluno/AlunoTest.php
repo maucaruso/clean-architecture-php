2fbc6dd9f64f7daf01bb52bb2079c9226fda8af3
maucaruso <carusojr@outlook.com>
2023-04-17 11:48:12 -0300

    test: Adding tests for Aluno Entity

---
+++ tests/Dominio/Aluno/AlunoTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PHP\Architecture\Tests\Dominio\Aluno;
+
+use PHP\Architecture\Dominio\Aluno\Aluno;
+use PHP\Architecture\Dominio\CPF;
+use PHP\Architecture\Dominio\Email;
+use PHPUnit\Framework\TestCase;
+
+class AlunoTest extends TestCase
+{
+  private Aluno $aluno;
+  
+  protected function setUp(): void
+  {
+    $this->aluno = new Aluno(
+      $this->createStub(CPF::class),
+      '',
+      $this->createStub(Email::class)
+    );
+  }
+  
+  public function testAdicionarMaisDe2TelefonesDeveLancarExcecao()
+  {
+    $this->expectException(\DomainException::class);
+    
+    $this->aluno->adicionarTelefone('24', '22222222');
+    $this->aluno->adicionarTelefone('24', '333333333');
+    $this->aluno->adicionarTelefone('24', '44444444');
+  }
+  
+  public function testAdicionar1TelefoneDeveFuncionar()
+  {
+    $this->aluno->adicionarTelefone('24', '22222222');
+    $this->assertCount(1, $this->aluno->telefones());
+  }
+  
+  public function testAdicionar2TelefonesDeveFuncionar()
+  {
+    $this->aluno->adicionarTelefone('24', '22222222');
+    $this->aluno->adicionarTelefone('24', '333333333');
+    $this->assertCount(2, $this->aluno->telefones());
+  }
+}
